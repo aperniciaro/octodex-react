b8cdeaf43a5a6eb7e58db75db06adab3aad16e53
Andrew Perniciaro <andrew.perniciaro@gmail.com>
2019-02-19 15:11:20 -0500

    fixed string concatenation

--- src/components/CatPic.js
+++ src/components/CatPic.js
@@ -5,31 +5,24 @@ class CatPic extends Component {
     const kitty = this.props.catName
     return (
       <figure>
-        <a href = "https://octodex.github.com//baracktocat" + {kitty}>
-          <img
-            class = "cat-pic"
-            src = "/images/" + {kitty} + ".jpg"
-            alt = {kitty}
-          />
+        <a href={'https://octodex.github.com//baracktocat' + kitty}>
+          <img class="cat-pic" src={'/images/' + kitty + '.jpg'} alt={kitty} />
         </a>
         <figcaption>
           <p>#001</p>
           <ul>
             <li>the</li>
             <li>
-              <a
-                class = "pic-name"
-                href = "https://octodex.github.com//" + {kitty}
-              >
+              <a class="pic-name" href={'https://octodex.github.com//' + kitty}>
                 {kitty.toUpperCase()}
               </a>
             </li>
             <li>by</li>
             <li>
-              <a href = "https://github.com/cameronmcefee">
+              <a href="https://github.com/cameronmcefee">
                 <img
-                  src = "/images/" + {kitty} + " author.jpeg"
-                  alt = {kitty} + " author"
+                  src={'/images/' + kitty + ' author.jpeg'}
+                  alt={kitty + ' author'}
                 />
               </a>
             </li>
