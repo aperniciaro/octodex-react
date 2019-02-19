0dcee597495e5cae60f40fae51822f6fbc141a18
Andrew Perniciaro <andrew.perniciaro@gmail.com>
2019-02-19 14:55:00 -0500

    reformatted code

--- src/components/CatPic.js
+++ src/components/CatPic.js
@@ -4,11 +4,11 @@ class CatPic extends Component {
   render() {
     return (
       <figure>
-        <a href=("https://octodex.github.com//baracktocat"+{this.props.catName})>
+        <a href = "https://octodex.github.com//baracktocat" + {this.props.catName}>
           <img
-            class="cat-pic"
-            src=("/images/"+{this.props.catName}+".jpg")
-            alt={this.props.catName}
+            class = "cat-pic"
+            src = "/images/" + {this.props.catName} + ".jpg"
+            alt = {this.props.catName}
           />
         </a>
         <figcaption>
@@ -17,18 +17,18 @@ class CatPic extends Component {
             <li>the</li>
             <li>
               <a
-                class="pic-name"
-                href="https://octodex.github.com//"+{this.props.catName}
+                class = "pic-name"
+                href = "https://octodex.github.com//" + {this.props.catName}
               >
                 {this.props.catname.toUpperCase()}
               </a>
             </li>
             <li>by</li>
             <li>
-              <a href="https://github.com/cameronmcefee">
+              <a href = "https://github.com/cameronmcefee">
                 <img
-                  src="/images/"+{this.props.catName}+" author.jpeg"
-                  alt={this.props.catName}+" author"
+                  src = "/images/" + {this.props.catName} + " author.jpeg"
+                  alt = {this.props.catName} + " author"
                 />
               </a>
             </li>
